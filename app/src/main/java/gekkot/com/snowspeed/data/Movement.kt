098dabac54b0428767e52177b92af651e9f38516
Gekko <HelloGekko@gmail.com>
2017-03-14 14:19:07 +0300

    add ormlite annotation to Movement class

--- app/src/main/java/gekkot/com/snowspeed/data/Movement.kt
+++ app/src/main/java/gekkot/com/snowspeed/data/Movement.kt
@@ -1,6 +1,11 @@
 package gekkot.com.snowspeed.data
 
 import android.location.Location
+import com.j256.ormlite.field.DatabaseField
+import com.j256.ormlite.table.DatabaseTable
+import gekkot.com.snowspeed.db.SQLRequestsGenerator
 
-
-class Movement(var time: Long, var location: Location, var rideId: Long)
+@DatabaseTable(tableName = SQLRequestsGenerator.POINTS_TABLE_NAME)
+class Movement(@DatabaseField var time: Long,
+               @DatabaseField var location: Location,
+               @DatabaseField var rideId: Long)
