322e8381b984bf933c834e1288a8e5998713d21f
Gekko <HelloGekko@gmail.com>
2017-03-14 15:33:11 +0300

    add latitude and longitude fields to Movement class

--- app/src/main/java/gekkot/com/snowspeed/data/Movement.kt
+++ app/src/main/java/gekkot/com/snowspeed/data/Movement.kt
@@ -6,6 +6,41 @@ import com.j256.ormlite.table.DatabaseTable
 import gekkot.com.snowspeed.db.SQLRequestsGenerator
 
 @DatabaseTable(tableName = SQLRequestsGenerator.POINTS_TABLE_NAME)
-class Movement(@DatabaseField var time: Long,
-               @DatabaseField var location: Location,
-               @DatabaseField var rideId: Long)
+class Movement {
+
+    @DatabaseField
+    var time: Long
+
+    var location: Location
+        set(value) {
+            this.latitude = value.latitude
+            this.longitude = value.longitude
+            this.location = value
+        }
+
+    @DatabaseField
+    var latitude: Double
+
+    @DatabaseField
+    var longitude: Double
+
+
+    @DatabaseField
+    var rideId: Long
+
+    constructor() {
+        this.time = 0L;
+        this.location = Location("");
+        this.rideId = 0;
+        this.latitude = 0.0
+        this.longitude = 0.0
+    }
+
+    constructor(time: Long, location: Location, rideId: Long) {
+        this.time = time
+        this.location = location
+        this.rideId = rideId
+        this.latitude = location.latitude
+        this.longitude = location.longitude
+    }
+}
