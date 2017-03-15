8cfc6ab26b19232cc44b8d883b271b08843e2c47
Gekko <HelloGekko@gmail.com>
2017-03-15 17:55:09 +0300

    add menu item to remove all movement points

--- app/src/main/java/gekkot/com/snowspeed/ui/MainActivity.java
+++ app/src/main/java/gekkot/com/snowspeed/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package gekkot.com.snowspeed.ui;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import gekkot.com.snowspeed.R;
+import gekkot.com.snowspeed.db.OrmLiteOpenHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,6 +47,11 @@ public class MainActivity extends AppCompatActivity {
         }
         if(id == R.id.action_map){
             MapsTrackActivity.startMapActivity(MainActivity.this);
+        }
+        if (id == R.id.action_clear_points) {
+            OrmLiteOpenHelper ormLiteOpenHelper = new OrmLiteOpenHelper(MainActivity.this);
+            SQLiteDatabase writableDatabase = ormLiteOpenHelper.getWritableDatabase();
+            ormLiteOpenHelper.recreateTables(writableDatabase);
         }
 
         return super.onOptionsItemSelected(item);
